90084e8a696ca1ebaeff4f5ad2cc7af5a2a68dbb
Forrest Zhao <fozhao92@gmail.com>
2017-05-03 20:42:23 -0700

    add uicolor extension util

---
+++ InstagramFirebaseClone/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  InstagramFirebaseClone
+//
+//  Created by Forrest Zhao on 5/3/17.
+//  Copyright © 2017 Forrest Zhao. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+}
